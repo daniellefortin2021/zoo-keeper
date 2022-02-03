01d3dd131dc917c67711453baaa305b574d44369
Danielle Fortin <fortind6@gmail.com>
2022-02-02 20:30:03 -0500

    added post

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 //add express to app varialbe so we can later chain on methods to the express server
 const app = express();
@@ -62,9 +64,62 @@ function findById(id, animalsArray) {
     } else {
       res.send(404);
     }
-  });
-  
-  app.listen(PORT, () => {
+});
+
+//function accepts post routes req.body value and array we want to add data to
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray}, null, 2)
+  );
+  //return finished code to post route for response
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if(!animal.name || typeof animal.name !== 'string'){
+    return false;
+  }
+  if(!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+
+  if(!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+
+  if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+
+  return true;
+}
+
+// another method of the app object that allows us to create routes - listening for POST requests
+app.post('/api/animals', (req, res) => {
+      //set id based on what the next index of the array will be 
+      req.body.id = animals.length.toString();
+
+      //if any data in req.body is incorrect, send 400 error baack
+      if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+      } else {
+        //add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+      }
+});
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+app.use(express.json());
+
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-  });
+});
   
